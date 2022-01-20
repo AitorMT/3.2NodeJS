d872debd4690ca313c937aaac1f868723b908a54
AMT <aitormunoz.alu@iespacomolla.es>
2022-01-20 21:31:02 +0100

    Terminado.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3977;
 const urlMongoAtlas =
   "mongodb+srv://admin:admin123456@aitordb.9iypo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
